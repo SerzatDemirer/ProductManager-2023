bb2523c7d3ceec068c1386da42aa6dde8dc83ab9
Serzat Demirer <91182016+SerzatDemirer@users.noreply.github.com>
2023-09-14 09:41:01 +0200

    Implemented and fixed AddProduct method, added migrations for database.

--- ProductManager-2023/Program.cs
+++ ProductManager-2023/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProductManager_2023
 {
     class Program
     {
+        static FashionDbContext DbContext = new FashionDbContext();
+
+
         static void Main(string[] args)
         {
             while (true)
@@ -24,13 +28,14 @@ namespace ProductManager_2023
                         break;
                     case "4":
                         ExitProgram();
-                        return;  // Avslutar programmet
+                        return;  // Avslutar program
                     default:
                         Console.WriteLine("Ogiltigt val, försök igen.");
                         break;
                 }
             }
         }
+
 
         private static void ShowMainMenu()
         {
@@ -43,19 +48,77 @@ namespace ProductManager_2023
             Console.WriteLine("Välj ett alternativ genom att ange siffran:");
         }
 
-        private static void AddProduct()
+
+        static void AddProduct()
         {
-            // Här lägger du till logiken för att lägga till en produkt
+            Console.Clear();
+            Console.WriteLine("Lägg till ny produkt");
+
+            Console.WriteLine("\nNamn:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("SKU:");
+            var sku = Console.ReadLine();
+
+            Console.WriteLine("Beskrivning:");
+            var description = Console.ReadLine();
+
+            Console.WriteLine("Bild (URL):");
+            var imageUrl = Console.ReadLine();
+
+            Console.WriteLine("Pris:");
+            decimal price;
+            while (!decimal.TryParse(Console.ReadLine(), out price) || price < 0)
+            {
+                Console.WriteLine("Ogiltigt pris. Ange ett giltigt pris:");
+            }
+
+            var product = new Product
+            {
+                Name = name,
+                SKU = sku,
+                Description = description,
+                ImageUrl = imageUrl,
+                Price = price
+            };
+
+            try
+            {
+                DbContext.Products.Add(product);
+                DbContext.SaveChanges();
+                Console.WriteLine("\nProdukt sparad!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ett fel inträffade när produkten skulle sparas. Detaljer: " + e.Message);
+            }
         }
+
 
         private static void SearchProduct()
         {
-            // Här lägger du till logiken för att söka efter en produkt
+            Console.WriteLine("Ange SKU eller produktens namn för att söka:");
+            var searchTerm = Console.ReadLine();
+
+            var product = DbContext.Products.FirstOrDefault(p => p.SKU == searchTerm || p.Name.Contains(searchTerm));
+
+            if (product != null)
+            {
+                Console.WriteLine($"Namn: {product.Name}");
+                Console.WriteLine($"SKU: {product.SKU}");
+                Console.WriteLine($"Beskrivning: {product.Description}");
+                Console.WriteLine($"Bild (URL): {product.ImageUrl}");
+                Console.WriteLine($"Pris: {product.Price} SEK");
+            }
+            else
+            {
+                Console.WriteLine("Ingen produkt med den SKU:n eller namnet hittades.");
+            }
+
         }
 
         private static void DeleteProduct()
         {
-            // Här lägger du till logiken för att radera en produkt
         }
 
         private static void ExitProgram()
